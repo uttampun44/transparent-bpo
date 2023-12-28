8b90f6e7814373d666d350b439d2172a0fa8347f
uttam <uttampun62@gmail.com>
2023-12-28 23:21:21 +0545

    hero section

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/index.css" >
+</head>
+<body>
+   <header>
+      <div class="header_container">
+           <div class="header_rows">
+                 <div class="header_logo">
+                     <img src="{{ asset('/images/transparent-bpo-logo 1.png')}}" />
+                 </div>
+                 <div class="header_login_findjobs">
+                       <div class="header_links">
+                            <a href="">Find Jobs</a>
+                            <a href="">News Login</a>
+                       </div>
+
+                       <div class="header_login">
+                          <a href=""> Login </a>
+                       </div>
+                 </div>
+           </div>
+      </div>
+   </header>
+</body>
+</html>
