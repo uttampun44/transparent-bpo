eb41cbb75b9c27ed2d25da589241af377033dc0e
uttam <uttampun62@gmail.com>
2024-03-01 21:07:16 +0545

    adding job description blade file

---
+++ resources/views/jobsdescription/jobsdescription.blade.php
@@ -0,0 +1,9 @@
+<x-header.header />
+<main>
+    <section>
+        <div class="container mx-auto jobsdescription">
+            <h1>Jobs Description</h1>
+        </div>
+    </section>
+</main>
+<x-footer.footer />
